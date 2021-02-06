3140f45153bdb9dcddc9b67d99178b9689bf43da
natyxflow <natyflores123@hotmail.com>
2021-02-05 23:25:04 -0300

    criando página inicial html

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./src/views")
 
 @app.route("/")
-def hoje():
-    return "Olá mundo!"
+def home():
+    return render_template("index.html")
+
+@app.route("/sobre")
+def sobre():
+    return "Sobre"
 
 app.run(port=8080, debug=True)    
